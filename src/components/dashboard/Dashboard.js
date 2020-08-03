05ab18076c9c4861f494bc059285b1ac122a14b5
Chase <Chase.sanville@gmail.com>
2020-08-02 19:05:18 -0700

    Added base Dashboard Component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Dashboard.css';
+
+class Dashbaord extends React.Component {
+    render() {
+        return <div className="Dashboard"> Dashboard Div</div>;
+    }
+}
+
+export default Dashbaord;
